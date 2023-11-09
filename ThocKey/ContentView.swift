459f976ac64a75ec82bc71b204df17a04689458e
JinJohnM <jin.jinnawat09@gmail.com>
2023-11-09 09:06:31 +0700

    Initial Commit

---
+++ ThocKey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThocKey
+//
+//  Created by Jinnawat Makwisai on 9/11/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThocKey/ThocKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThocKeyApp.swift
+//  ThocKey
+//
+//  Created by Jinnawat Makwisai on 9/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ThocKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
